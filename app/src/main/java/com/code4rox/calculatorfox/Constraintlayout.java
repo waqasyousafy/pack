3c35b43baf8da57cd084309fec04a12ee804142d
waqas yousaf <waqasyousafy@gmail.com>
2019-09-23 15:46:44 +0500

    Spinner items attached and their color changed

--- app/src/main/java/com/code4rox/calculatorfox/Constraintlayout.java
+++ app/src/main/java/com/code4rox/calculatorfox/Constraintlayout.java
@@ -3,12 +3,39 @@ package com.code4rox.calculatorfox;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class Constraintlayout extends AppCompatActivity {
+public class Constraintlayout extends AppCompatActivity implements  AdapterView.OnItemSelectedListener {
 
+    String[] Speedformat={"KM/h","M/h"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_constraintlayout);
+
+
+        Spinner spin=(Spinner) findViewById(R.id.dropdown_speed);
+        spin.setOnItemSelectedListener(this);
+        ArrayAdapter aa=new ArrayAdapter(this,R.layout.support_simple_spinner_dropdown_item,Speedformat);
+        aa.setDropDownViewResource(R.layout.spinner_color);
+        spin.setAdapter(aa);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        TextView textView = (TextView)parent.getChildAt(0);
+        textView.setTextColor(getResources().getColor(R.color.colorgolden));
+        Toast.makeText(getApplicationContext(),Speedformat[position] , Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
